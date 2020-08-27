0c0352102eb07f35b4e01d73179cf6d4bc88c5ac
barisayyildiz <barisayyildiz.1799@gmail.com>
2020-08-27 15:47:22 +0300

    changed database from nedb to mongoose added a delete button

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
 const app = express();
 const expbs = require("express-handlebars");
-const DataStore = require("nedb");
+const mongoose = require("mongoose");
+var methodOverride = require('method-override')
 
-let database = new DataStore({filename : "database.db"});
-database.loadDatabase();
+mongoose.connect("mongodb://localhost/blog");
+
+const schema = mongoose.Schema(
+{
+	title : String,
+	text : String,
+	date : String,
+	dateMil : Number
+});
+
+
+const posts = mongoose.model('posts', schema);
 
 
 app.listen(3000, () => console.log("listening port number 3000..."));
@@ -13,22 +24,31 @@ app.listen(3000, () => console.log("listening port number 3000..."));
 app.use(express.static('./views/'));
 app.use(express.json({limit : "1mb"}));
 
-/*
+// Method override
+app.use(methodOverride('_method'));
+
 //Body Parsing
 
 app.use(express.urlencoded({
     extended : false
 }));
 app.use(express.json());
-*/
+
 
 // Setting the view engine, express-handlebars
 app.engine('handlebars', expbs());
 app.set('view engine', 'handlebars');
 
 // Dynamic page that shows all the blog posts
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
 
+	let docs = await posts.find({}).lean();
+	docs = sortByDateTime(docs);
+	console.log(docs);
+	res.render('index', {blogs : docs});
+
+
+	/*
 	database.find({}, (err, docs) => {
 
 		// Sorts the blog entries by the time
@@ -36,6 +56,7 @@ app.get("/", (req, res) => {
 
 		res.render('index', {blogs : docs});
 	})
+	*/
 
 })
 
@@ -49,13 +70,40 @@ app.post("/submit", (req, res) => {
 	// Generates DD/MM/YYYY format
 	let string = `${date.getDate()}/${date.getMonth()+1}/${date.getFullYear()}`;
 
-	database.insert({title : req.body.title, text : req.body.text, date : string, dateMil : Date.now()});
+	//database.insert({title : req.body.title, text : req.body.text, date : string, dateMil : Date.now()});
 
+
+	posts.create(
+	{
+		title : req.body.title,
+		text : req.body.text,
+		date : string,
+		dateMil : Date.now()
+	})
 
 });
 
+app.use((req, res, next) => 
+{
+	console.log(req.params);
+	next();
+})
+
+app.delete("/delete/:id", (req, res) => {
+
+	console.log("asd");
+
+	posts.remove({_id : req.params.id}, (err ,docs) => {
+		res.redirect("/");
+	})
+
+	return;
+});
+
+
 function sortByDateTime(database)
 {
+	
 	let temp;
 	for(let i=0; i<database.length; i++)
 	{
@@ -69,6 +117,7 @@ function sortByDateTime(database)
 			}
 		}
 	}
+	
 
 	return database;
 }
---
+++ views/script.js
@@ -0,0 +1,20 @@
+
+/*
+document.getElementById("dltbtn").addEventListener("click", (event) => {
+
+	fetch("/submit", {
+
+		method: "POST",
+		headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+	});
+
+})
+*/
+
+function deleteItem(node)
+{
+	console.log(node.myattr);
+}
